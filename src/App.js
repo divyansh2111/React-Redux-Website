1ecbed131ee60150705b5618f3993c6e09791d3a
divyansh2111 <divsriva@gmail.com>
2022-05-27 10:26:39 +0530

    React Components-1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import React, { Component } from 'react' 
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color='primary'>
         <div className='container'>
           <NavbarBrand href='/'>
@@ -13,6 +14,7 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
