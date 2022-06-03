a171abf80bc99808106d7b9ad5b93173ade4ca06
divyansh2111 <divsriva@gmail.com>
2022-06-03 21:25:13 +0530

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -106,7 +107,7 @@ class CommentForm extends Component {
             return(
                 <Card>
                     {console.log(dish)}
-                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {Breadcrumb, BreadcrumbItem, Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'; 
 import { Loading } from './LoadingComponent';
-// import DishDetail from './DishDetailComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
     function RenderMenuItem({dish}){
         return (
             <Card key={dish.id}>
                 <Link to={`/menu/${dish.id}`}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
+                  <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
                   </CardImgOverlay>
